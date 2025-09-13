76fe2cdf91fa345609c17892233f4b5c1841c1c8
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-13 22:01:05 +0500

    Сделанная папка с классами.Дегтянников

---
+++ KINO_Degtinnikov/Modell/Afisha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KINO_Degtinnikov.Modell
+{
+    public class Afisha
+    {
+        public int Id { get; set; }
+        public int IdKinoteatr { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Price { get; set; }
+        public Afisha(int Id, int IdKinoteatr, string Name, DateTime Time, int Price)
+        {
+            Id = Id;
+            IdKinoteatr = IdKinoteatr;
+            Name = Name;
+            Time = Time;
+            Price = Price;
+        }
+    }
+}
---
+++ KINO_Degtinnikov/Modell/Kinoteatr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KINO_Degtinnikov.Modell
+{
+    public class Kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set; }
+        public int Count { get; set; }
+        public Kinoteatr(int Id, string Name, int CountZal,int Count) 
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Count = Count;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+    }
+}
